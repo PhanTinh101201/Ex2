ff2495718139a01612af41499e2398fceff23c4d
Phan Tinh <you@example.com>
2022-07-22 17:15:21 +0700

    first commit

---
+++ src/components/ListItems/Index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import Indexitems from '../Items/Indexitems'
+
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Index = () => {
+  return (
+    <Container>
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+     <Indexitems Title = 'Task 1' Creator = 'Author' Desscription = 'This is a task, This is a task, This is a task, This...' />
+    </Container>
+  )
+}
+
+export default Index
